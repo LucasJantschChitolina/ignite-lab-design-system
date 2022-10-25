f360690c30949691d5af4952fc76fa5d9750c074
unknown <lucas.chitolina01@gmail.com>
2022-10-25 07:21:17 -0300

    Adding tests to Storybook

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { SignIn } from './SignIn'
+import { SignIn } from './SignIn';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 export default {
   title: 'Pages/SignIn',
@@ -8,4 +10,17 @@ export default {
   argTypes: {}
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  play: async({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(canvas.getByPlaceholderText("Digite seu e-mail"), 'diego@rocketseat.com.br');
+    userEvent.type(canvas.getByPlaceholderText("***********"), '123456');
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login realizado!")).toBeInTheDocument(); 
+    })
+  }
+}
